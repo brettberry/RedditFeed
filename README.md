a9b431fa78bfca2407e6db157a65910e4d268fe6
Brett Berry <brett.elizabeth.berry@gmail.com>
2017-03-24 11:35:05 -0700

    initial commit w/ boilerplate

--- README.md
+++
@@ -1,4 +0,0 @@
-Web boilerplate
----------------
-
-A simple front end boilerplate with Babel, Webpack, Radium, and React.
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div className="container">
-                <h1 className="name">Brett Berry</h1>
+                <h1 className="header">Test</h1>
             </div>
         );
     }
